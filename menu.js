f7ed60d4d89a9c45538e7ea302ac7aed3a6e1d44
jordangonen <jordangonen@wustl.edu>
2016-04-25 18:37:24 -0500

    updated with JS

--- menu.js
+++ menu.js
@@ -3,11 +3,43 @@ var order = '';
 
 
 // build a function to ask questions
+var answers = [
+  order
+];
+document.write("<p> You Ordered : </p>");
+
 
 function question(questionText, selection) {
     //what our function will do
-    order = prompt("What do you want to eat?");
-    toppings = prompt("What kinds of toppings would you like on that?");
+    var questions = [
+      "rice",
+      "chicken" ,
+      "beans" ,
+      "cheese" ,
+      "steak" ,
+      "lettuce" ,
+      "sour cream" ,
+
+
+    ];
+    for (i=0; i<questions.length; i++){
+      order += prompt(questions[i]);
+      if (order == "YES"){
+        answers +=questions[i];
+
+      }
+      document.write("<p>" + questions[i] + "</p>");
+
+        }
+
+
+
+
+    // toppings = prompt("What kinds of toppings would you like on that?");
+
+
+
+
 
 
 
@@ -15,9 +47,9 @@ function question(questionText, selection) {
 
   // your code goes here
 }
+document.write(answers);
 
 // call the QUESTION function with the order options
 question();
 
 // output their order
-document.write("<p>You ordered " + order + " with " +toppings + " on the side.<p>");
